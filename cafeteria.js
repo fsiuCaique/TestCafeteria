41c15bcb3e38e1bec8a63620b8a6c99b3107b7ae
fsiuCaique <89097038+fsiuCaique@users.noreply.github.com>
2025-06-28 17:01:05 -0300

    Add files via upload

---
+++ cafeteria.js
@@ -0,0 +1,40 @@
+let currentIndex = 0;
+let totalItems = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('cafeteria.json')
+        .then(response => response.json())
+        .then(itens => {
+            const carrossel = document.getElementById('carrossel');
+            totalItems = itens.length;
+
+            itens.forEach(item => {
+                const div = document.createElement('div');
+                div.classList.add('cafeteira');
+                div.innerHTML = `
+                    <img src="${item.imagem}" alt="${item.imagem}">
+                    <h2>${item.nome}</h2>
+                    <p>${item.descricao}</p>
+                `;
+                carrossel.appendChild(div);
+            });
+
+            updateCarousel(); // inicializa
+        });
+
+    document.getElementById('prev').addEventListener('click', () => {
+        currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+        updateCarousel();
+    });
+
+    document.getElementById('next').addEventListener('click', () => {
+        currentIndex = (currentIndex + 1) % totalItems;
+        updateCarousel();
+    });
+});
+
+function updateCarousel() {
+    const carrossel = document.getElementById('carrossel');
+    carrossel.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
